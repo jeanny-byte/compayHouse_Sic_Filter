372937bb2938eb30e581c991de81f293a65a5ae4
jeanny-byte <jeanclaude17052000@gmail.com>
2023-10-21 11:20:36 +0000

    edited 1M file

--- RobSic1M.py
+++ RobSic1M.py
@@ -2,14 +2,24 @@ import os
 import csv
 from tqdm import tqdm
 
-def search_and_append(input_file, output_file):
+def extract_company_data(input_file):
+    company_data = set()
     with open(input_file, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            # Modify this section to perform your search for SIC codes or company names.
-            # For example, you can check if the SIC code or company name is in 'row'.
-            # If the row qualifies, write it to the output file.
-            if 'your_search_condition' in row:
+            # Extract the Company Name and Company Number from each row
+            company_name = row[0]  # Assuming Company Name is in the first column (index 0)
+            company_number = row[1]  # Assuming Company Number is in the second column (index 1)
+            company_data.add(company_name)
+            company_data.add(company_number)
+    return company_data
+
+def search_and_append(input_file, output_file, target_data):
+    with open(input_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            # Check if either the Company Name or Company Number exists in the row
+            if row[0] in target_data or row[1] in target_data:
                 with open(output_file, 'a', newline='') as output_file:
                     writer = csv.writer(output_file)
                     writer.writerow(row)
@@ -18,6 +28,9 @@ def main():
     folder_path = '/Results'  # Replace with the path to your folder
     csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
     output_file_name = 'Rob&Filtered_date.csv'
+    
+    # Extract Company Name and Company Number from the 1m_net.csv file
+    target_data = extract_company_data('1m_net.csv')
 
     # Create the output file if it doesn't exist
     if not os.path.exists(output_file_name):
@@ -29,7 +42,7 @@ def main():
     # Create a progress bar for the file processing
     for csv_file in tqdm(csv_files, desc="Processing CSV files"):
         input_file_path = os.path.join(folder_path, csv_file)
-        search_and_append(input_file_path, output_file_name)
+        search_and_append(input_file_path, output_file_name, target_data)
 
 if __name__ == "__main__":
     main()
